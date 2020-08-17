393d25b5f440d5e5b9168e642fd974ec4af26eef
Romulo Conceição Silva <57760111+RomulloSilva@users.noreply.github.com>
2020-08-17 20:52:38 -0300

    Inicio do curso API RESTfull

---
+++ biblioteca-rs/src/main/java/com/bibliotecars/BibliotecaRsApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotecars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaRsApplication.class, args);
+	}
+
+}
