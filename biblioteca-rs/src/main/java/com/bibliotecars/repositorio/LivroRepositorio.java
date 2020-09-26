7c34ec8674a8668c06775463eb83c251673466f7
Romulo Conceição Silva <57760111+RomulloSilva@users.noreply.github.com>
2020-09-26 17:55:02 -0300

    Correção do repositório do Pedido

--- biblioteca-rs/src/main/java/com/bibliotecars/repositorio/LivroRepositorio.java
+++ biblioteca-rs/src/main/java/com/bibliotecars/repositorio/LivroRepositorio.java
@@ -3,6 +3,7 @@ package com.bibliotecars.repositorio;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bibliotecars.domain.Livro;
--- biblioteca-rs/src/main/java/com/bibliotecars/repositorio/PedidoRepositorio.java
+++ biblioteca-rs/src/main/java/com/bibliotecars/repositorio/PedidoRepositorio.java
@@ -1,5 +1,17 @@
 package com.bibliotecars.repositorio;
 
-public interface PedidoRepositorio {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bibliotecars.domain.Pedido;
+
+@Repository
+public interface PedidoRepositorio extends JpaRepository<Pedido, Long> {
+
+	/*
+	 * Método para retorna todos os livros dentro do pedido.
+	 */
+	public List<Pedido> findAllByLivro(Long id);
 }
